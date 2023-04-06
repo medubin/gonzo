3e819666799815679919f5b2bc9494ab2a352982
medubin <medubin@gmail.com>
2023-04-06 16:10:32 -0700

    Fix URL

--- api/src/url/utils.go
+++ api/src/url/utils.go
@@ -32,7 +32,7 @@ func GetTypedParamsFromContext[Params any](ctx context.Context) Params {
 	}
 
 	for key, value := range ctx.Value(ParamKey{}).(map[string]string) {
-		reflect.ValueOf(&params).Elem().FieldByName(key).SetString(value)
+		reflect.ValueOf(&params).Elem().FieldByName(key).Set(reflect.ValueOf(&value))
 	}
 	return params
 }
